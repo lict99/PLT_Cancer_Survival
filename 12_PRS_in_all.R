3ae486a291bd8e253d0e883dbf54694056e06ec5
CT-Li <60499240+CT-Li@users.noreply.github.com>
2023-07-22 21:15:29 +0800

    refine PRS calculation and plotting

--- 12_PRS_in_all.R
+++ 12_PRS_in_all.R
@@ -1,44 +1,43 @@
-
-# env settings ------------------------------------------------------------
+# env settings ----
 
 library(magrittr)
+library(survival)
 library(ggplot2)
-library(ggsci)
-library(scales)
-library(cowplot)
+library(patchwork)
 
 load("11/IV_info.RData")
-load("11/functions_gg_prs.RData")
 load("01/UKB_all_info.RData")
-load("01/whole_cancer_data_for_Cox.RData")
+
+source("functions/PRS_plot.R")
 
 ukb_snp_ind <- read.csv("src/SNP/UKb_snp_individual.csv")
 ukb_snp_ind_supp <- read.csv("src/SNP/UKb_snp_individual_supp.csv")
 
 dir.create("12", FALSE)
 
-# calculation -------------------------------------------------------------
+# calculation ----
 
 snp_ind <- merge(
-  ukb_snp_ind, 
+  ukb_snp_ind,
   ukb_snp_ind_supp,
   by = "eid",
   all = TRUE
-) %>% 
-  extract(, c("eid", iv_info$All_sites$affy)) %>% 
-  set_colnames(c("eid", iv_info$All_sites$SNP))
+) %>%
+  extract(, c("eid", iv_info$affy)) %>%
+  set_colnames(c("eid", iv_info$SNP))
 
-snp_score <- data.frame(eid = snp_ind$eid) %>% 
+snp_score <- data.frame(eid = snp_ind$eid) %>%
   {
-    for (i in iv_info$All_sites$SNP) {
+    score <- .
+    for (i in iv_info$SNP) {
       eff <- subset(
-        iv_info$All_sites, 
-        SNP == i, 
+        iv_info,
+        SNP == i,
         "effect_allele.exposure",
         drop = TRUE
       )
       oth <- subset(
-        iv_info$All_sites,
+        iv_info,
         SNP == i,
         "other_allele.exposure",
         drop = TRUE
@@ -46,7 +45,7 @@ snp_score <- data.frame(eid = snp_ind$eid) %>%
       homo_eff <- paste(eff, eff)
       homo_oth <- paste(oth, oth)
       heto <- c(paste(eff, oth), paste(oth, eff))
-      
+
       col <- snp_ind[, i]
       for (j in seq_along(col)) {
         if (!is.na(col[j])) {
@@ -63,30 +62,42 @@ snp_score <- data.frame(eid = snp_ind$eid) %>%
           }
         }
       }
-     .[, i] <- as.numeric(col)
+      score[, i] <- as.numeric(col)
     }
-    .
+    score
   }
 
 prs_all <- data.frame(
   eid = snp_score$eid,
   prs_u = rowSums(snp_score[, -1]),
   prs_w = (function() {
-    if (identical(names(snp_score[, -1]), iv_info$All_sites$SNP)) {
+    if (identical(names(snp_score[, -1]), iv_info$SNP)) {
       a <- as.matrix(snp_score[, -1])
-      b <- matrix(iv_info$All_sites$beta.exposure, ncol = 1)
+      b <- matrix(iv_info$beta.exposure, ncol = 1)
     } else {
       stop("Not identical in names!")
     }
     a %*% b
   })()
-) %>% 
-  merge(UKb_baseline[, c("eid", "platelet")], by = "eid", all.x = TRUE) %>% 
+) %>%
+  merge(UKb_baseline[, c("eid", "platelet")], by = "eid", all.x = TRUE) %>%
   na.omit()
 
-prs_without_cancer <- subset(
-  prs_all,
-  !is.element(prs_all$eid, whole_cancer_data$All_sites$eid)
+# plotting ----
+
+prs_w_plot <- gg_prs(
+  data = prs_all,
+  annotate = "All participants in UK biobank",
+  x_col = "prs_w"
 )
 
-gg_prs(prs_all, "prs_u", "all participants")
+prs_u_plot <- gg_prs(
+  data = prs_all,
+  annotate = "All participants in UK biobank",
+  x_col = "prs_u"
+)
+
+# plots saving ----
+
+ggsave("12/weighted_PRS_in_all.pdf", prs_w_plot)
+ggsave("12/unweighted_PRS_in_all.pdf", prs_u_plot)
